b6816c6f036c14502c47bcd17d54f9553e91d3d6
johnhorsema <jcema@connect.ust.hk>
2019-07-21 16:52:08 +0000

    Decentralized!

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,171 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Todo Dapp, written in JavaScript + Rust (WebAssembly)</h2>
+    <p>Powered by <a href="https://wavelet.perlin.net">Wavelet</a>. Click <a href="https://medium.com/perlin-network/build-a-decentralized-chat-using-javascript-rust-webassembly-c775f8484b52">here</a> to learn how it works, and <a href="https://github.com/perlin-network/decentralized-chat">here</a> for the source code. Join our <a href="https://discord.gg/dMYfDPM">Discord</a> to get PERLs.</p>
+    <input class="nes-input" placeholder="Add todo..." v-on:keyup.enter="addTodo"/>
+    <div class="spacer"></div>
+    <ol>
+      <li v-for="(todo, idx) in todos">
+        <label>
+          <input type="checkbox"
+            class="nes-checkbox"
+            @click="toggleTodo(idx)"
+            :checked="todo.done">
+
+          <del v-if="todo.done">
+            {{ todo.content }}
+          </del>
+          <span v-else>
+            {{ todo.content }}
+          </span>
+          <button class="nes-btn is-error" v-on:click="removeTodo(idx)">&times;</button>
+        </label>
+      </li>
+    </ol>
+    <hr/>
+    <h3>Logs</h3>
+    <textarea readonly>{{ log.join("\n") }}</textarea>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue';
+import JSBI from "jsbi";
+import {Wavelet,Contract} from "wavelet-client";
+const BigInt = JSBI.BigInt;
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  mounted() {
+    this.init();
+  },
+  data() {
+    return {
+      wallet: null,
+      contract: null,
+      todos: [],
+      log: []
+    }
+  },
+  methods: {
+    async init() {
+      var self = this
+      return await this.$contract.init().then(resp => {
+        self.getTodos()
+        self.listen()
+      })
+    },
+    async listen() {
+      var self = this
+      await this.$client.pollConsensus({
+        onRoundEnded: msg => {
+          (async () => {
+              await self.$contract.fetchAndPopulateMemoryPages();
+                self.getTodos();
+          })();
+        }
+      });
+    },
+    getTodos() {
+      var raw = this.$contract.test('get_todos', BigInt(0));
+      this.todos = raw.logs[0].split('\n').reverse().map((a, aidx) => {
+        var matched = a.split(' ');
+        return {
+          id: aidx,
+          content: matched[0].replace(/[\<\>]/g, ''),
+          done: eval(matched[1])
+        }
+      });
+    },
+    async addTodo({target}) {
+      var self = this
+      return await this.$contract.call(
+        this.$wallet, 
+        'add_todo', 
+        BigInt(0), 
+        BigInt(250000),
+        {type: "string", value: target.value},
+      ).then(resp => {
+        target.value = '';
+        self.log.push(resp.tx_id);
+      })
+    },
+    async removeTodo(id) {
+      var self = this
+      return await this.$contract.call(
+        this.$wallet, 
+        'remove_todo', 
+        BigInt(0), 
+        BigInt(250000),
+        {type: "uint32", value: id},
+      ).then(resp => {
+        self.log.push(resp.tx_id);
+      })
+    },
+    async toggleTodo(id) {
+      var self = this
+      return await this.$contract.call(
+        this.$wallet, 
+        'toggle_todo', 
+        BigInt(0), 
+        BigInt(250000),
+        {type: "uint32", value: id},
+      ).then(resp => {
+        self.log.push(resp.tx_id);
+      })
+    }
   }
 }
 </script>
 
 <style>
+html, body, pre, code, kbd, samp {
+  font-family: "Press Start 2P";
+}
+
+body {
+  background: #20262E;
+  padding: 20px;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+  transition: all 0.2s;
+  margin: 2rem;
+  padding: 2rem;
+}
+
+ol {
+  margin-left: 16px;
+}
+
+li {
+  margin: 8px 0;
+}
+
+h2 {
+  font-weight: bold;
+  margin-bottom: 15px;
+}
+
+del {
+  color: rgba(0, 0, 0, 0.3);
+}
+
+.nes-btn {
+  padding: 0px 4px;
+  margin: 0px;
+}
+
+.spacer {
+  height: 1rem;
+}
+
+textarea {
+  width: 100%;
+  border: none;
+  height: 200px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {Wavelet,Contract} from "wavelet-client";
 
 Vue.config.productionTip = false
+
+const client = new Wavelet('https://testnet.perlin.net');
+const wallet = Wavelet.loadWalletFromPrivateKey("43f5699a788ab63376f683b23921102c33ba7aa90da34b1d146100a04312056b2f24a207ac8c253948c0aad85b6a0a56cda0191dc65bb6730158ff8e6e18c6c5");
+const contract = new Contract(client, "97f293b44e7b9f1d000d45e0f44059d35526f4367546f9c919ba2d0f154f1001");
+
+Vue.use({
+  install (Vue) {
+    Vue.prototype.$contract = contract
+    Vue.prototype.$wallet = wallet
+    Vue.prototype.$client = client
+  }
+})
 
 new Vue({
   render: h => h(App),
