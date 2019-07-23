62644137d5dcebeee583fbe751d4a883419e05cc
johnhorsema <jcema@connect.ust.hk>
2019-07-23 07:19:32 +0000

    Fix sentences.

--- src/App.vue
+++ src/App.vue
@@ -71,7 +71,7 @@ export default {
       var raw = this.$contract.test('get_todos', BigInt(0));
       if(raw.logs[0].indexOf('\n') !== -1) {
         this.todos = raw.logs[0].split('\n').reverse().map((a, aidx) => {
-          var matched = a.split(' ');
+          var matched = a.split('> ');
           return {
             id: aidx,
             content: matched[0].replace(/[\<\>]/g, ''),
